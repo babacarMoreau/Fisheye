479ed761dae25ed7eca365bd1f6a349e1b0f933a
Babacar Moreau <babacar.openclassroom@gmail.com>
2024-05-17 16:40:17 +0200

    recup data photographe

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,29 +1,13 @@
     async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+    
+        const reponse = await fetch("data/photographers.json");
+
+        const photographerAndMedia = await reponse.json();
+
+        const photographers = photographerAndMedia.photographers
+
+        return { photographers };  
+
     }
 
     async function displayData(photographers) {
@@ -38,6 +22,7 @@
 
     async function init() {
         // Récupère les datas des photographes
+
         const { photographers } = await getPhotographers();
         displayData(photographers);
     }
