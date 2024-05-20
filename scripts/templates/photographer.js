28832b495a50f0ed18ec5c1c40e9e171ddbf201f
Babacar Moreau <babacar.openclassroom@gmail.com>
2024-05-20 20:37:21 +0200

    cm1

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,5 +1,5 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -9,8 +9,23 @@ function photographerTemplate(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+
+        const p = document.createElement('p');
+        p.textContent = city + ', ' + country;
+
+        const p1 = document.createElement('p');
+        p1.textContent = tagline;
+
+        const p2 = document.createElement('p');
+        p2.textContent = price + "€/jour"
+
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(p);
+        article.appendChild(p1);
+        article.appendChild(p2);
+        
         return (article);
     }
     return { name, picture, getUserCardDOM }
